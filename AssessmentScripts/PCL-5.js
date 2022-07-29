da34547341529267c075566784875be58473feb1
Brandon Clyde <bclyde@kbbh.org>
2022-07-29 12:50:52 -0700

    Create PCL-5 subform script

---
+++ AssessmentScripts/PCL-5.js
@@ -0,0 +1,18 @@
+var pcl5Score;
+$('document').ready(function () {
+  $('tr')
+    .has('div[class=untouchable]')
+    .find('input')
+    .prop('readOnly', 'readOnly');
+  $('input[type=button]').click(function () {
+    pcl5Score = 0;
+    $('input[name*=_calc]').each(function () {
+      if (this.value) {
+        pcl5Score += parseInt(this.value);
+      }
+    });
+    $('tr').has('div[id=answer]').find('input').val(pcl5Score);
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {linkValueToExtFrame(parent.document, 'pcl5Score', '#ticEvaluation', '#pcl5Score');  });
