145bb6eee68b6b5759977fcf5d478cab7e924645
Brandon Clyde <bclyde@kbbh.org>
2022-07-29 12:16:47 -0700

    Create ACE subform script

---
+++ AssessmentScripts/ACE.js
@@ -0,0 +1,23 @@
+var loopcount;
+var aceScore;
+$('tr').has('div[class=answer]').find('input').prop('readOnly', 'readOnly');
+function countQuestions() {
+  for (
+    loopcount = 0, aceScore = 0;
+    loopcount < $('tr').find('div[class=question]').length;
+    loopcount++
+  ) {
+    if (
+      $('tr').has('div[class=question]').find('input')[loopcount * 2].checked ==
+      true
+    ) {
+      aceScore++;
+    }
+  }
+  $('tr').has('div[class=answer]').find('input').val(aceScore);
+}
+$(document).ready(function () {
+  $('tr').has('div[class=question]').find('input').click(countQuestions);
+});
+
+document.addEventListener('DOMContentLoaded', () => {linkValueToExtFrame(parent.document, 'aceScore', '#ticEvaluation', '#aceScore');  });
