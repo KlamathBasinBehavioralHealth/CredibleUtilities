8be554b2d14d759a35105bfd2fd4753e93e22119
Brandon Clyde <bclyde@kbbh.org>
2022-07-29 11:14:13 -0700

    Reorder FormAggregate.js

--- FormAggregate.js
+++ FormAggregate.js
@@ -1,50 +1,3 @@
-let mainHref;
-function setHref(context, formName, submitBool) {
-  const navTree = context.querySelector('a.form_yellow').closest('tbody');
-  navTree.querySelectorAll('a').forEach((link) => {
-    if (link.textContent != formName) {
-      if (submitBool) {
-        link.href = refList[camelCase(link.textContent)];
-      } else {
-        link.href = 'javascript:void(0)';
-      }
-    } else {
-      if (submitBool) {
-        link.href = mainHref;
-      } else {
-        mainHref = link.href;
-        link.href = 'javascript:void(0)';
-      }
-    }
-  });
-}
-function hideSubforms(context, folderText, bool) {
-  const subformParent = findLinkFromText(folderText, context).parentElement;
-  if (bool) {
-    subformParent.hidden = true;
-  } else {
-    subformParent.hidden = false;
-  }
-}
-function setLinks(context, folderText) {
-  const links = findLinkFromText(
-    folderText,
-    context
-  ).parentElement.querySelectorAll('a.form_yellow');
-  links.forEach((link) => {
-    link.onclick = (e) => {
-      const frame = document.querySelector('#' + camelCase(link.textContent));
-      frame.scrollIntoView();
-      if (
-        frame.contentWindow.document.querySelector('#questions_container')
-          .hidden
-      ) {
-        frame.contentWindow.document.querySelector('.toolHead').click();
-      }
-    };
-  });
-}
-
 const sudRefList = {
   initialInformation:
     'https://www.cbh3.crediblebh.com/webforms/questions.asp?category_id=132855',
@@ -188,6 +141,53 @@ function setFrameHeightEvents() {
     iframe.style.height = idealFrameHeight(iframe);
     iframe.contentWindow.document.body.onclick = () => {
       iframe.style.height = idealFrameHeight(iframe);
+    };
+  });
+}
+
+let mainHref;
+function setHref(context, formName, submitBool) {
+  const navTree = context.querySelector('a.form_yellow').closest('tbody');
+  navTree.querySelectorAll('a').forEach((link) => {
+    if (link.textContent != formName) {
+      if (submitBool) {
+        link.href = refList[camelCase(link.textContent)];
+      } else {
+        link.href = 'javascript:void(0)';
+      }
+    } else {
+      if (submitBool) {
+        link.href = mainHref;
+      } else {
+        mainHref = link.href;
+        link.href = 'javascript:void(0)';
+      }
+    }
+  });
+}
+function hideSubforms(context, folderText, bool) {
+  const subformParent = findLinkFromText(folderText, context).parentElement;
+  if (bool) {
+    subformParent.hidden = true;
+  } else {
+    subformParent.hidden = false;
+  }
+}
+function setLinks(context, folderText) {
+  const links = findLinkFromText(
+    folderText,
+    context
+  ).parentElement.querySelectorAll('a.form_yellow');
+  links.forEach((link) => {
+    link.onclick = (e) => {
+      const frame = document.querySelector('#' + camelCase(link.textContent));
+      frame.scrollIntoView();
+      if (
+        frame.contentWindow.document.querySelector('#questions_container')
+          .hidden
+      ) {
+        frame.contentWindow.document.querySelector('.toolHead').click();
+      }
     };
   });
 }
