4bd4dc0c38f83f802d04c37e55242e20b05889de
Yu Hsu <yhsu@kbbh.org>
2022-09-12 09:28:45 -0700

    Made it so visibility can accept any selector.

--- Utilities/visibility.js
+++ Utilities/visibility.js
@@ -1,50 +1,29 @@
 //New require function
 function requireField (target, condition) { 
-	$('tr').find('div[class*=' + target + ']').next().remove(); 
-	$('tr').find('div[id=' + target + ']').next().remove();  
+	$('tr').find(target).next().remove();  
+  document.querySelector(target).nextElementSibling.remove();
 	if(condition) { 
-		$('tr').has('div[class*=' + target + ']').find('input').prop('required', true); 
-		$('tr').has('div[class*=' + target + ']').find('select').prop('required', true); 
-		$('tr').find('div[class*=' + target + ']').after('<div class=\'redAsterisk\' style=\'color : red; display : inline\'>*</div>');
-		$('tr').has('div[id=' + target + ']').find('input').prop('required', true); 
-		$('tr').has('div[id=' + target + ']').find('select').prop('required', true); 
-		$('tr').find('div[id=' + target + ']').after('<div class=\'redAsterisk\' style=\'color : red; display : inline\'>*</div>');  
+		$('tr').has(target).find('input').prop('required', true); 
+		$('tr').has(target).find('select').prop('required', true); 
+		$('tr').find(target).after('<div class=\'redAsterisk\' style=\'color : red; display : inline\'>*</div>');  
 	} 
 	else { 
-		$('tr').has('div[class*=' + target + ']').find('input').prop('required', false); 
-		$('tr').has('div[class*=' + target + ']').find('select').prop('required', false); 
-		$('tr').has('div[id=' + target + ']').find('input').prop('required', false); 
-		$('tr').has('div[id=' + target + ']').find('select').prop('required', false);
+		$('tr').has(target).find('input').prop('required', false); 
+		$('tr').has(target).find('select').prop('required', false); 
 	} 
 }
 
 //New hide show function
 function visibility(hideShow, target, require = false){
   if(hideShow == 'show'){
-    $('.' + target).closest('table')?.parent().parent().show();
-    $('.' + target).closest('table')?.parent().parent().each(function (){
-      $(this).nextAll('tr:has(img[height=15]):first').show();
-    });
-    $('#' + target).closest('table')?.parent().parent().show();
-    $('#' + target).closest('table')?.parent().parent().each(function (){
-      $(this).nextAll('tr:has(img[height=15]):first').show();
-    });
-    $('div[hide=' + target + ']')?.closest('table').parent().parent().show();
-    $('div[hide=' + target + ']')?.closest('table').parent().parent().each(function (){
+    $(target).closest('table')?.parent().parent().show();
+    $(target).closest('table')?.parent().parent().each(function (){
       $(this).nextAll('tr:has(img[height=15]):first').show();
     });
   }
   else if(hideShow == 'hide'){
-    $('.' + target).closest('table')?.parent().parent().hide();
-    $('.' + target).closest('table')?.parent().parent().each(function (){
-      $(this).nextAll('tr:has(img[height=15]):first').hide();
-    });
-    $('#' + target).closest('table')?.parent().parent().hide();
-    $('#' + target).closest('table')?.parent().parent().each(function (){
-      $(this).nextAll('tr:has(img[height=15]):first').hide();
-    });
-    $('div[hide=' + target + ']')?.closest('table').parent().parent().hide();
-    $('div[hide=' + target + ']')?.closest('table').parent().parent().each(function (){
+    $(target).closest('table')?.parent().parent().hide();
+    $(target).closest('table')?.parent().parent().each(function (){
       $(this).nextAll('tr:has(img[height=15]):first').hide();
     });
   }
