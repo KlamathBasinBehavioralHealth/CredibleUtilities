04982e514af82a5b2ca3da8292d660cb14bb3b7f
Yu Hsu <yhsu@kbbh.org>
2022-09-28 16:23:28 -0700

    Swapping to bootstrap tooltip.

--- AssessmentScripts/DLA20.js
+++ AssessmentScripts/DLA20.js
@@ -14,9 +14,9 @@ function checkAge(){
   
   $('#dla20NA').closest('table').find('input').css('pointer-events', 'auto');
   $('#dla20NA').closest('table').find('input').attr('tabindex', naTabIndex);
-  $('#dla20NA').closest('table').attr('title', '');
-  $('#dla20NA').closest('table').attr('data-toggle', '');
-  
+  $('#dla20NA').closest('tr').attr('title', '');
+  $('#dla20NA').closest('tr').attr('data-toggle', '');
+  $('#dla20NA').closest('tr').tooltip('disabled');
 
   if(clientAge < 6  && clientAge != ' '){
     visibility('hide', '.dla20Q', false);
@@ -26,9 +26,9 @@ function checkAge(){
       $('#dla20NA').closest('table').find('input').trigger('click');
       $('#dla20NA').closest('table').find('input').css('pointer-events', 'none');
       $('#dla20NA').closest('table').find('input').attr('tabindex', -1);
-      $('#dla20NA').closest('table').attr('title', 'Client is under the age of 6.');
-      $('#dla20NA').closest('table').attr('data-toggle', 'tooltip');
-      
+      $('#dla20NA').closest('tr').attr('title', 'Client is under the age of 6.');
+      $('#dla20NA').closest('tr').attr('data-toggle', 'tooltip');
+      $('#dla20NA').closest('tr').tooltip();
     }
   }
   else{
