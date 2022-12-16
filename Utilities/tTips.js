dbaa5ef21b3d9614381118f4919fcac17cce61af
Yu Hsu <yhsu@kbbh.org>
2022-12-16 11:24:46 -0800

    Tooltips

--- Utilities/tTips.js
+++ Utilities/tTips.js
@@ -2,7 +2,7 @@ let tTipStyle = null;
 let tTipStyleSheet = null;
 
 $('document').ready(function () {
-    tTipStyle = '.tTip{ position: relative; cursor: pointer; } .tTipText{ position: absolute; width: 50vw; text-align: center; left: 50%; top: 0; transform: translateX(25%); background-color: black; color: white; white-space: normal; padding: 10px 15px; border-radius: 7px; visibility: hidden; opacity: 0; transition: opacity 0.5s ease; } .tTipText::before{ content: \'\'; position: absolute; left: 0%; transform: translateX(-125%); border: 7px solid; border-color: #0000 #000 #0000 #0000; } .tTip:hover .tTipText{ left: 50%; visibility: visible; opacity: 1; }';
+    tTipStyle = '.tTip{ position: relative; cursor: pointer; } .tTipText{ position: absolute; width: 50vw; text-align: center; left: 50%; top: 0; transform: translateX(25%); background-color: black; color: white; white-space: normal; padding: 10px 15px; border-radius: 7px; visibility: hidden; opacity: 0; transition: opacity 0.5s ease; } .tTipText::before{ content: \'\'; position: absolute; left: 0%; transform: translateX(-90%); border: 7px solid; border-color: #0000 #000 #0000 #0000; } .tTip:hover .tTipText{ left: 50%; visibility: visible; opacity: 1; }';
 
     tTipStyleSheet = document.createElement('style'); 
     tTipStyleSheet.innerText = tTipStyle; 
