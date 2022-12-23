e000fa6b85f0ea83d6cd22baac72826a6c28ca4c
Yu Hsu <yhsu@kbbh.org>
2022-12-22 17:36:41 -0800

    CSSRS version 3

--- AssessmentScripts/CSSRS.js
+++ AssessmentScripts/CSSRS.js
@@ -261,3 +261,135 @@ $('document').ready(function () {
 });
 
 document.addEventListener('DOMContentLoaded', () => {linkValueToExtFrame(parent.document, 'cssrsScore', '.frame', '#cssrsScore');  });
+
+
+/*
+
+else if(
+      $('tr')
+        .has('div[id=cssrsQ4], div[id=cssrsQ5]')
+        .find("tr:contains('Yes')")
+        .find('input:even:checked').length) 
+    {
+      $('tr')
+        .has('div[id=urgency]')
+        .find('select')
+        .val(
+          $('tr')
+            .has('div[id=urgency]')
+            .find('option')
+            .filter(function () {
+              return $(this).html() == 'Emergent (Immediate access)';
+            })
+            .val()
+        );
+      $('tr')
+        .has('div[id=motsUrgency]')
+        .find('select')
+        .val(
+          $('tr')
+            .has('div[id=motsUrgency]')
+            .find('option')
+            .filter(function () {
+              return $(this).html() == 'Emergent (Immediate access)';
+            })
+            .val()
+        );
+      cssrsScore = 'Emergent (Immediate access)';
+    } else if(
+      $('tr')
+        .has('div[id=cssrsQ3]')
+        .find("tr:contains('Yes')")
+        .find('input:even:checked').length &&
+      !$('tr')
+        .has('div[id=cssrsQ4], div[id=cssrsQ5]')
+        .find("tr:contains('Yes')")
+        .find('input:even:checked').length)
+      {
+        $('tr')
+          .has('div[id=urgency]')
+          .find('select')
+          .val(
+            $('tr')
+              .has('div[id=urgency]')
+              .find('option')
+              .filter(function () {
+                return $(this).html() == 'Urgent (Access within 24 business hours)';
+              })
+              .val()
+          );
+        $('tr')
+          .has('div[id=motsUrgency]')
+          .find('select')
+          .val(
+            $('tr')
+              .has('div[id=motsUrgency]')
+              .find('option')
+              .filter(function () {
+                return $(this).html() == 'Urgent (Access within 24 business hours)';
+              })
+              .val()
+          );
+        cssrsScore = 'Urgent (Access within 24 business hours)';
+      } else if (
+        !$('tr')
+          .has('div[id=cssrsQ3], div[id=cssrsQ4], div[id=cssrsQ5]')
+          .find("tr:contains('Yes')")
+          .find('input:even:checked').length) 
+      {
+        $('tr')
+          .has('div[id=urgency]')
+          .find('select')
+          .val(
+            $('tr')
+              .has('div[id=urgency]')
+              .find('option')
+              .filter(function () {
+                return $(this).html() == 'Routine (Access within 10 business days)';
+              })
+              .val()
+          );
+        $('tr')
+          .has('div[id=motsUrgency]')
+          .find('select')
+          .val(
+            $('tr')
+              .has('div[id=motsUrgency]')
+              .find('option')
+              .filter(function () {
+                return $(this).html() == 'Routine (Access within 10 business days)';
+              })
+              .val()
+          );
+        cssrsScore = 'Routine (Access within 10 business days)';
+      }
+      if (
+        !$('tr').has('div[id=cssrsQ1], div[id=cssrsQ2], div[id=cssrsQ6]').find('input:checked')
+          .length)
+      {
+        $('tr')
+          .has('div[id=urgency]')
+          .find('select')
+          .val(
+            $('tr')
+              .has('div[id=urgency]')
+              .find('option')
+              .filter(function () {
+                return $(this).html() == '';
+              })
+              .val()
+          );
+        $('tr')
+          .has('div[id=motsUrgency]')
+          .find('select')
+          .val(
+            $('tr')
+              .has('div[id=motsUrgency]')
+              .find('option')
+              .filter(function () {
+                return $(this).html() == '';
+              })
+              .val()
+          );
+      }
+*/
