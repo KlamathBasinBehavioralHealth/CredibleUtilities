ad4de58a8be8558c37e04f03dd66cdd5c5948b0e
Yu Hsu <yhsu@kbbh.org>
2022-12-22 17:23:59 -0800

    CSSRS version 3

--- AssessmentScripts/CSSRS.js
+++ AssessmentScripts/CSSRS.js
@@ -247,6 +247,10 @@ $('document').ready(function () {
     .has('div[id=cssrsQ1], div[id=cssrsQ2], div[id=cssrsQ3], div[id=cssrsQ6]')
     .find('input')
     .change(cssrsWorkflow);
+  $('tr')
+    .has('div[id=cssrsQ1], div[id=cssrsQ2], div[id=cssrsQ3], div[id=cssrsQ6]')
+    .find('input')
+    .mouseleave(cssrsWorkflow);
   $('tr').has('div[class=cssrsQ]').find('input').change(calculateCSSRS);
 });
 
