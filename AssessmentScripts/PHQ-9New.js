01ab717e0afb3496735277f8773326d820a4e4b2
Yu Hsu <yhsu@kbbh.org>
2023-07-03 14:45:51 -0700

    Testing Age 0.

--- AssessmentScripts/PHQ-9New.js
+++ AssessmentScripts/PHQ-9New.js
@@ -27,12 +27,15 @@ function checkAge(){
   visibility('hide', '.phq9AdultQ', false);
   visibility('hide', '.phq9AdolescentQ', false);
   visibility('hide', '.phq9Q', false);
+  visibility('hide', '#phq9A', false);
   if(age >= 18){
     visibility('show', '.phq9AdultQ', true);
     visibility('show', '.phq9Q', true);
+    visibility('show', '#phq9A', false);
   }else if(age < 18 && age >= 11){
     visibility('show', '.phq9AdolescentQ', true);
     visibility('show', '.phq9Q', true);
+    visibility('show', '#phq9A', false);
   }
 }
 
