c70aba03b3606c656b0be10ddaa81e29e265c072
exploosion <81261400+exploosion@users.noreply.github.com>
2023-11-03 13:03:07 -0700

    Default Drop Downs even if lookup.

---
+++ Utilities/defaultDropDown.js
@@ -0,0 +1,27 @@
+function defaultDropDown(target, condition){
+    let options = target.querySelectorAll('option');
+    let targetValue = undefined;
+    try{
+        targetValue = [...options].filter((option) => {
+            return option.innerText === condition;
+        })[0].value;
+    }catch(error){
+        console.log(error);
+    }
+
+    if(target.value === '' && targetValue !== undefined){
+        target.value = targetValue;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    [...document.querySelectorAll('.defaultDropDown')].forEach((div) => {
+        try{
+            let select = div.closest('table').querySelector('select');
+            let condition = div.getAttribute('defaultCondition');
+            defaultDropDown(select, condition);
+        }catch(error){
+            console.log(error);
+        }
+    });
+});
