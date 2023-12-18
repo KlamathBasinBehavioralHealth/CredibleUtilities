1e3ddd9336e8a6c64c78dca9d201567c617cf97a
Yu Hsu <yhsu@kbbh.org>
2023-12-18 11:35:30 -0800

    First commit

---
+++ Utilities/requireCheckboxes.js
@@ -0,0 +1,51 @@
+function isChecked(question){
+    let checked = false;
+    question.closest('table').querySelectorAll('input').forEach((input) => {
+      if(input.checked){
+          checked = true;
+      }
+    });
+    return checked;
+  }
+  
+  function markUnansweredQuestion(question, mode = 'mark'){
+    let newID = `unansweredQuestion${question.innerText.replace(/\s+/g, '')}`;
+    if(mode === 'mark'){
+      const newDiv = document.createElement('div');
+      newDiv.setAttribute('id', newID);
+      newDiv.innerText = `Please select at least one option for '${question.innerText}'.`;
+      newDiv.style.color = 'red';
+    }else if(mode === 'unmark'){
+      document.querySelector(`#${newID}`)?.remove();
+    }
+    return newID;
+  }
+  
+  function setupRequireCheckboxes(){
+    document.querySelector('form').addEventListener('submit', (event) => {
+      let validated = true;
+      
+      document.querySelectorAll('.requireCheckbox').forEach((element) => {
+        let unmarkedQuestionID = undefined;
+        unmarkedQuestionID = markUnansweredQuestion(element, 'unmark');
+        if(!isChecked(element)){
+          validated = false;
+          unmarkedQuestionID = markUnansweredQuestion(element, 'mark');
+          document.querySelector(`#${newID}`).scrollIntoView();
+        }
+      });
+      
+      if(!validated){
+        event.preventDefault();
+        console.log('Checkboxs not answered!');
+      }
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    if(document.querySelector('input[name=Complete]').disabled){
+      document.querySelector('input[name=Complete]').disabled = false;
+    }
+    setupRequireCheckboxes();
+  });
+  
