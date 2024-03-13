6458e49bd5ef25d88b7d4e720556f23223937a96
exploosion <81261400+exploosion@users.noreply.github.com>
2024-03-13 09:59:33 -0700

    Update hideableElements.js

--- Utilities/hideableElements.js
+++ Utilities/hideableElements.js
@@ -11,7 +11,7 @@ if(document.querySelector('[name=Complete]')){
       document.getElementsByTagName('head').item(0).appendChild(script);
     }
   
-    include('https://cdn.jsdelivr.net/gh/KlamathBasinBehavioralHealth/CredibleUtilities@21229c2/Utilities/visibility.js');
+    include('https://cdn.jsdelivr.net/gh/KlamathBasinBehavioralHealth/CredibleUtilities@6d2f7be/Utilities/visibility.js');
   }
 }
 
