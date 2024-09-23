7ef6938cae717d019af9442c7d4b94dda493db2c
Yu Hsu <thereisnospoon@bleepscythe.com>
2024-09-23 16:49:10 -0700

    Checking for Visibility Function

--- Utilities/crisisCalculator.js
+++ Utilities/crisisCalculator.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   
     include('https://cdn.jsdelivr.net/gh/KlamathBasinBehavioralHealth/CredibleUtilities/Utilities/visibility.js');
   }
-  
+
   if($('tr').has('div[id=clientAge]').find('input')?.val() != ''){
 		age = $('tr').has('div[id=clientAge]').find('font').text();
 		console.log(age);
@@ -80,8 +80,9 @@ document.addEventListener('DOMContentLoaded', async () => {
   }
 	
   crisisCalculatorURL = `https://cors-everywhere.azurewebsites.net/reportservices.crediblebh.com/reports/ExportService.asmx/ExportXML?connection=LYEC1uwvr-7RAoxbT4TJDuiO!gY1p8-aFVdERsxbI0e8PCfuSwWEdENelwx!LcKy&start_date=&end_date=&custom_param1=${tempVisitID}&custom_param2=&custom_param3=`;
-    visibility('hide', '.mcisQ');
-    visibility('hide', '.mcisA');
+  
+  visibility('hide', '.mcisQ');
+  visibility('hide', '.mcisA');
 
   try{
     crisisCalculatorResult = await getData(crisisCalculatorURL);
