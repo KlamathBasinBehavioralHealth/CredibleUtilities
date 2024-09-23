7c946b504ac0fc452c2f0cd36303da4595077b6e
Yu Hsu <yhsu@kbbh.org>
2024-09-23 12:40:52 -0700

    Checking if getData results are 0 and ignoring.

--- Utilities/crisisCalculator.js
+++ Utilities/crisisCalculator.js
@@ -83,16 +83,16 @@ document.addEventListener('DOMContentLoaded', async () => {
     billingMatrixID = crisisCalculatorResult.querySelector('billing_matrix_id').innerHTML;
     emLevel = crisisCalculatorResult.querySelector('em_level').innerHTML;
     
-    if(cptCodeTarget.value !== '0'){
+    if(cptCodeTarget.value){
       cptCodeTarget.value = cptCode;
     }
-    if(modifier1Target.value !== '0'){
+    if(modifier1Target.value){
       modifier1Target.value = modifier1;
     }
-    if(billingMatrixIDTarget.value !== '0'){
+    if(billingMatrixIDTarget.value){
       billingMatrixIDTarget.value = billingMatrixID;
     }
-    if(emLevelTarget.value !== '0'){
+    if(emLevelTarget.value){
       emLevelTarget.value = emLevel;
     }
   }catch(error){
