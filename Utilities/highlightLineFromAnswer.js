757c8878ffe75f0b2e11befb9c2d6bff19361ed5
SBThach <sthach@kbbh.org>
2024-10-16 16:19:33 -0700

    Update highlightLineFromAnswer.js

--- Utilities/highlightLineFromAnswer.js
+++ Utilities/highlightLineFromAnswer.js
@@ -34,16 +34,17 @@ var questionHighlights = {};
 		addRecordToColumn(questionHighlights, lineToHighlight, question);
 	}
 });
+
+
+//Function below will check if any question that has the same 'lineToHighlight' indicates a positive for highlighting
 var positiveIndicator = false;
 function checkPreviousQuestions(lineToHighlight) {
-	console.log('checkHighlights Triggered');
 	var questionCheckRadioAnswers;
 	var questionDropdownAnswers;
 	var dropdownQuestion;
 	var lineToHighlight;
 	var answerTrigger;
 	try{
-		console.log('checkHighlights Trying');
 		questionHighlights[lineToHighlight].forEach(question => {
 			qtype = question.getAttribute('qtype');
 			if(qtype == 'radio' || qtype == 'checkbox'){
@@ -55,7 +56,6 @@ function checkPreviousQuestions(lineToHighlight) {
 						positiveIndicator = true;
 					}
 				});
-				console.log('Radio/Checkbox Highlighting End');
 			} else if (qtype == 'dropdown'){
 				console.log('Dropdown Highlighting Started');
 				dropdownQuestion = question.closest('tr').nextElementSibling.querySelector('select');
@@ -66,15 +66,14 @@ function checkPreviousQuestions(lineToHighlight) {
 						positiveIndicator = true;
 					}
 				});
-				console.log('DropDown Highlighting End');
 			}
 		});
-		console.log('checkHighlights End');
 	} catch(error){
 		console.log(error);
 	}
 }
 
+//This function actually looks at the question that was checked and highlights according to the function checkPreviousQuestions's positive indicator
 function checkHighlights(lineToHighlight) {
 	positiveIndicator = false;
 	console.log('checkHighlights Triggered');
@@ -90,13 +89,13 @@ function checkHighlights(lineToHighlight) {
 			checkPreviousQuestions(lineToHighlight);
 			lineToHighlight = Array.from(document.querySelectorAll('tr:has(div#' + question.getAttribute('highlightThis')));
 			qtype = question.getAttribute('qtype');
-			if(qtype == 'radio' || qtype == 'checkbox'){
+			if(qtype == 'radio' || qtype == 'checkbox'){ // If statement for radio and checkboxes since they act different from drop downs
 				console.log('Radio/Checkbox Highlighting Started');
-				questionCheckRadioAnswers = Array.from(question.closest('tbody').querySelector('tbody').querySelectorAll('input'));
-				answerTrigger = question.getAttribute('answerTriggers').split(',').map(Number);
-				answerDeTrigger = range(0, questionCheckRadioAnswers.length - 1).filter(item => !answerTrigger.includes(item));
+				questionCheckRadioAnswers = Array.from(question.closest('tbody').querySelector('tbody').querySelectorAll('input')); // The actual radio buttons/checkboxes themselves
+				answerTrigger = question.getAttribute('answerTriggers').split(',').map(Number); // Array for the answers that can highlight
+				answerDeTrigger = range(0, questionCheckRadioAnswers.length - 1).filter(item => !answerTrigger.includes(item)); // Array for non-answers
 				answerDeTrigger.forEach(index => {
-					if (questionCheckRadioAnswers[index].checked && !positiveIndicator) {
+					if (questionCheckRadioAnswers[index].checked && !positiveIndicator) { // Checks if one of the non-answers are checked and if the positive indicator is false
 						lineToHighlight.forEach(row => {
 							row.style.backgroundColor = 'white';
 							console.log('De-Highlighted');
@@ -104,7 +103,7 @@ function checkHighlights(lineToHighlight) {
 					}
 				});
 				answerTrigger.forEach(index => {
-					if (questionCheckRadioAnswers[index].checked && positiveIndicator) {
+					if (questionCheckRadioAnswers[index].checked && positiveIndicator) { // Checks if one of the answers are checked
 						lineToHighlight.forEach(row => {
 							row.style.backgroundColor = 'yellow';
 							console.log('Highlighted');
@@ -112,15 +111,15 @@ function checkHighlights(lineToHighlight) {
 					}
 				});
 				console.log('Radio/Checkbox Highlighting End');
-			} else if (qtype == 'dropdown'){
+			} else if (qtype == 'dropdown'){ // Dropdowns act different so we need an else if statement to read dropdowns
 				console.log('Dropdown Highlighting Started');
-				dropdownQuestion = question.closest('tr').nextElementSibling.querySelector('select');
-				questionDropdownAnswers = Array.from(question.closest('tr').nextElementSibling.querySelectorAll('option'));
-				answerTrigger = question.getAttribute('answerTriggers').split(',').map(Number);
-				answerDeTrigger = range(0, questionDropdownAnswers.length - 1).filter(item => !answerTrigger.includes(item));
+				dropdownQuestion = question.closest('tr').nextElementSibling.querySelector('select'); // The dropdown itself
+				questionDropdownAnswers = Array.from(question.closest('tr').nextElementSibling.querySelectorAll('option')); // The actual drop down options
+				answerTrigger = question.getAttribute('answerTriggers').split(',').map(Number); // Array for the answers that can highlight
+				answerDeTrigger = range(0, questionDropdownAnswers.length - 1).filter(item => !answerTrigger.includes(item));// Array for non-answers
 								console.log('Dropdown Highlighting');
 				answerDeTrigger.forEach(index => {
-					if (dropdownQuestion.selectedIndex == index && positiveIndicator) {
+					if (dropdownQuestion.selectedIndex == index && positiveIndicator) { // Checks if one of the non-answers are checked and if the positive indicator is false
 						lineToHighlight.forEach(row => {
 							row.style.backgroundColor = 'white';
 							console.log('De-Highlighted');
@@ -128,7 +127,7 @@ function checkHighlights(lineToHighlight) {
 					}
 				});
 				answerTrigger.forEach(index => {
-					if (dropdownQuestion.selectedIndex == index) {
+					if (dropdownQuestion.selectedIndex == index) { // Checks if one of the answers are checked
 						lineToHighlight.forEach(row => {
 							row.style.backgroundColor = 'yellow';
 							console.log('Highlighted');
@@ -151,8 +150,8 @@ $(document).ready(function() {
 				var lineToHighlight = input.closest('td').closest('table').closest('tr').previousElementSibling.querySelector('.highlightQuestions').getAttribute('highlightthis');
 				checkHighlights(lineToHighlight);
 			} else if(input.tagName.toLowerCase() == 'select'){
-				
+				var lineToHighlight = input.closest('tr').closest('tr').querySelector('select');
+				checkHighlights(lineToHighlight);
 			}
 		});
 	});
-});
