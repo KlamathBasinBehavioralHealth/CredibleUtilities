c7e9d228a45d4b647a3c46af16a9d3518aa93f67
SBThach <sthach@kbbh.org>
2024-10-16 16:29:35 -0700

    Update highlightLineFromAnswer.js

--- Utilities/highlightLineFromAnswer.js
+++ Utilities/highlightLineFromAnswer.js
@@ -150,7 +150,7 @@ $(document).ready(function() {
 				var lineToHighlight = input.closest('td').closest('table').closest('tr').previousElementSibling.querySelector('.highlightQuestions').getAttribute('highlightthis');
 				checkHighlights(lineToHighlight);
 			} else if(input.tagName.toLowerCase() == 'select'){
-				var lineToHighlight = input.closest('tr').closest('tr').querySelector('select').getAttribute('highlightthis');;
+				var lineToHighlight = input.closest('tr').closest('tr').querySelector('select').querySelector('.highlightQuestions').getAttribute('highlightthis');
 				checkHighlights(lineToHighlight);
 			}
 		});
