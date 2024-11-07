df11229aba76e6df6445b6dd12d48dddd628c16b
SBThach <sthach@kbbh.org>
2024-11-07 15:39:52 -0800

    Update treatmentPlanTemplate.js

--- TreatmentPlanTemplate/treatmentPlanTemplate.js
+++ TreatmentPlanTemplate/treatmentPlanTemplate.js
@@ -83,9 +83,23 @@ try{
 
 async function stopParagraphButton(targetBody){
   const observer = new MutationObserver((mutationsList) => {
-    dumb_span = document.querySelector('#cke_1_contents > iframe').contentWindow.document.body.firstElementChild.tagName == 'SPAN';
-	dumb_span.remove();
-
+    //dumb_span = document.querySelector('#cke_1_contents > iframe').contentWindow.document.body.firstElementChild.tagName == 'SPAN';
+	//dumb_span.remove();
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        // Loop through added nodes in the mutation record
+        mutation.addedNodes.forEach(node => {
+          // Check if the added node is a span element
+		  if (node.nodeName === 'SPAN') {
+			contenteditable = node.getAttribute('contenteditable');
+		  }
+          if (node.nodeName === 'SPAN') {
+            console.log('New span detected and removed:', node);
+            node.remove(); // Delete the span element
+          }
+        });
+      }
+    }
   });
   
   // Configure the observer to watch for child nodes being added to the body
@@ -299,5 +313,3 @@ function overrideTemplateValidator(){
 function restoreTemplateValidator(){
   document.querySelector('#txPlanModule').contentDocument.querySelector('input[type=submit]').addEventListener('click', templateValidator);
 }
-
-
