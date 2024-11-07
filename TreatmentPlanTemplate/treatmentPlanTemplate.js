969e340dadfd97a3bef8dba9d0f8ffeb32b3ed79
SBThach <sthach@kbbh.org>
2024-11-07 15:01:35 -0800

    Update treatmentPlanTemplate.js

--- TreatmentPlanTemplate/treatmentPlanTemplate.js
+++ TreatmentPlanTemplate/treatmentPlanTemplate.js
@@ -89,10 +89,7 @@ async function stopParagraphButton(targetBody){
         // Loop through added nodes in the mutation record
         mutation.addedNodes.forEach(node => {
           // Check if the added node is a span element
-		  if(node.nodeName === 'SPAN'){
-			contenteditable = node.getAttribute('contenteditable')
-		  }
-          if (node.nodeName === 'SPAN' && contenteditable != null && contenteditable == 'false') {
+          if (node.nodeName === 'SPAN') {
             console.log('New span detected and removed:', node);
             node.remove(); // Delete the span element
           }
