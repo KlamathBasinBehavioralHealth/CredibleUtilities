3a6c81aaaf3a8d30360066c19ef4c095a1777642
SBThach <sthach@kbbh.org>
2025-01-10 13:32:59 -0800

    Swapping to specific visibility verison.

--- Utilities/hideableElements.js
+++ Utilities/hideableElements.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
       document.getElementsByTagName('head').item(0).appendChild(script);
     }
   
-    include('https://cdn.jsdelivr.net/gh/KlamathBasinBehavioralHealth/CredibleUtilities/Utilities/visibility.js');
+    include('https://cdn.jsdelivr.net/gh/KlamathBasinBehavioralHealth/CredibleUtilities@43438be/Utilities/visibility.js');
   }
 });
 
