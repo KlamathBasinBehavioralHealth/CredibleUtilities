cfde4120ec3a6bbe501ebdd264ea509ee48a9ad0
SBThach <sthach@kbbh.org>
2025-01-10 14:32:57 -0800

    Update visibility.js

--- Utilities/visibility.js
+++ Utilities/visibility.js
@@ -47,50 +47,6 @@ function requireField (target, condition) {
 	}
 }
 
-function checkRequiredCB(){
-	let cbCheckedCount = 0;
-	let cbQuestionCheckedCount = 0;
-	let firstCBUnchecked;
-	shownRequiredCBAnswers.forEach(answerArray => {
-		answerArray.forEach(answer => {
-			if(answer.checked){
-				cbCheckedCount = cbCheckedCount + 1;
-			}
-		});
-		if(cbCheckedCount > 0){
-			cbQuestionCheckedCount = cbQuestionCheckedCount + 1;
-		} 
-	});
-	if(cbQuestionCheckedCount < shownRequiredCBAnswers.length){
-		try{
-			document.querySelector('[name=Complete]').setAttribute('disabled',true);
-		}
-		catch(error){
-			console.log(error);
-		}
-		try{
-			document.querySelector('[value=Complete]').setAttribute('disabled',true);
-		}
-		catch(error){
-			console.log(error);
-		}		
-	}
-	else{
-		try{
-			document.querySelector('[name=Complete]').removeAttribute('disabled');
-		}
-		catch(error){
-			console.log(error);
-		}
-		try{
-			document.querySelector('[value=Complete]').removeAttribute('disabled');
-		}
-		catch(error){
-			console.log(error);
-		}		
-	}
-}
-
 //New hide show function
 function visibility(hideShow, target, require = false){
 	shownRequiredCBAnswers = [];
