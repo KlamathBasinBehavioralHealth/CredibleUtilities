aa53fb85f3aecdb85eceac4a2eb77abaa08b6213
SBThach <sthach@kbbh.org>
2025-01-10 14:02:15 -0800

    Visibility version

--- Utilities/hideableElements.js
+++ Utilities/hideableElements.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
       document.getElementsByTagName('head').item(0).appendChild(script);
     }
   
-    include('https://cdn.jsdelivr.net/gh/KlamathBasinBehavioralHealth/CredibleUtilities@5950643/Utilities/visibility.js');
+    include('https://cdn.jsdelivr.net/gh/KlamathBasinBehavioralHealth/CredibleUtilities@cbefcd5/Utilities/visibility.js');
   }
 });
 
