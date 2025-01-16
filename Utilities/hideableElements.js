c2ec0047bfeec5f303696b2e85629824721ace4c
SBThach <sthach@kbbh.org>
2025-01-16 14:53:08 -0800

    Update hideableElements.js

--- Utilities/hideableElements.js
+++ Utilities/hideableElements.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
       document.getElementsByTagName('head').item(0).appendChild(script);
     }
   
-    include('https://cdn.jsdelivr.net/gh/KlamathBasinBehavioralHealth/CredibleUtilities@72a3660/Utilities/visibility.js');
+    include('https://cdn.jsdelivr.net/gh/KlamathBasinBehavioralHealth/CredibleUtilities@02fcbc4/Utilities/visibility.js');
   }
 });
 
