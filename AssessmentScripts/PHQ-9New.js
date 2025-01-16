2da8ddf830088658dca77570a4582bdbc3ba66a7
SBThach <sthach@kbbh.org>
2025-01-16 09:16:45 -0800

    Update PHQ-9New.js

--- AssessmentScripts/PHQ-9New.js
+++ AssessmentScripts/PHQ-9New.js
@@ -9,7 +9,7 @@ if(typeof visibilty != 'function'){
     document.getElementsByTagName('head').item(0).appendChild(script);
   }
 
-  include('https://cdn.jsdelivr.net/gh/KlamathBasinBehavioralHealth/CredibleUtilities@0da3d17/Utilities/visibility.js');
+  include('https://cdn.jsdelivr.net/gh/KlamathBasinBehavioralHealth/CredibleUtilities@5996df8/Utilities/visibility.js');
 }
 
 let age;
