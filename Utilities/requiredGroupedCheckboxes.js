bf384149367dac40256bc758b0e79c2b53f3aa87
SBThach <sthach@kbbh.org>
2025-01-22 11:41:41 -0800

    Requiring No Label Checkboxes

---
+++ Utilities/requiredGroupedCheckboxes.js
@@ -0,0 +1,89 @@
+
+
+
+const groups = {};
+const requiredIndividualCheckboxes = [...document.querySelectorAll('.requireIndividualCheckboxes')];
+requiredIndividualCheckboxes.forEach(element => {
+  const groupId = element.getAttribute('groupid');
+  if (!groups[groupId]) {
+    groups[groupId] = [];
+  }
+  groups[groupId].push(element);
+});
+
+const requiredGroupCheckboxes = Object.values(groups);
+
+
+
+requiredGroupCheckboxes.forEach(checkboxGroup => {
+	const asterisk = document.createElement('span');
+	asterisk.textContent = '*';
+	asterisk.style.color = 'red'; 
+	searchClosestLabel(checkboxGroup).parentElement.appendChild(asterisk);
+    checkboxGroup.forEach(question =>{
+		$('tr').find(question).after('<div class=\'redAsterisk\' style=\'color : red; display : inline\'>*</div>');  
+	});
+});
+
+var closestB;
+function searchClosestLabel(checkboxGroup){
+	let currentRow = checkboxGroup[0].closest('table').closest('tr');  
+	let previousRow = currentRow.previousElementSibling; 
+	while (previousRow) {
+		const bElement = previousRow.querySelector('b');
+		previousRow = previousRow.previousElementSibling;
+		if (bElement) {
+			closestB = bElement;
+			previousRow = null;
+			return bElement;
+		}
+	}
+}
+
+
+var closestUncheckedRequiredLabel;
+function checkRequiredCB(){
+	const completeButtonName = document.querySelector('[name=Complete]');
+    const completeButtonValue = document.querySelector('[value=Complete]');
+	let cbCheckedCount = 0;
+	let checkedRequiredGroupCount = 0;
+	shownRequiredCBAnswers.forEach(checkboxGroup => {
+		let requiredCount = 1;
+		cbCheckedCount = 0;
+		checkboxGroup.forEach(answer => {
+			requiredCount = parseInt(answer.getAttribute('requiredCount'), 10);	
+			if(answer.checked){
+				cbCheckedCount = cbCheckedCount + 1;
+			}
+		});
+		if(cbCheckedCount >= requiredCount){
+			checkedRequiredGroupCount = checkedRequiredGroupCount + 1;
+		} 
+		else{
+			closestUncheckedRequiredLabel = searchClosestLabel(checkboxGroup);
+		}
+	});
+	if(checkedRequiredGroupCount < shownRequiredCBAnswers.length){
+		if(completeButtonName){
+			completeButtonName.setAttribute('disabled',true);
+		}
+		if(completeButtonValue){
+			completeButtonValue.setAttribute('disabled',true);
+		}	
+	}
+	else{
+		if(completeButtonName){
+			completeButtonName.removeAttribute('disabled');
+		}
+		if(completeButtonValue){
+			completeButtonValue.removeAttribute('disabled');
+		}	
+	}
+}
+
+//function checkRequirement() {
+//  const interval = 250; 
+//  const flashInterval = setInterval(() => {
+//	  
+//  }, interval);
+//}
