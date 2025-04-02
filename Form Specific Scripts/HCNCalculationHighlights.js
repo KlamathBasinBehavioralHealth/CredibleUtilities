7f946d500cbcabf34c416ad44abbe4d6e111b67b
SBThach <sthach@kbbh.org>
2025-04-02 10:29:14 -0700

    Update HCNCalculationHighlights.js

--- Form Specific Scripts/HCNCalculationHighlights.js
+++ Form Specific Scripts/HCNCalculationHighlights.js
@@ -91,9 +91,4 @@ $(document).ready(function() {
 			safetyHighlight();
 		});
 	});
-	mentalHealthCalc.forEach((input) => {
-		input.addEventListener('change', () => {
-			mentalHealthHighlight();
-		});
-	});
 });
