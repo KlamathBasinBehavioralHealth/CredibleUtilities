9667f82524783077dc6385b292e234c555cbf4af
SBThach <sthach@kbbh.org>
2025-04-09 11:18:37 -0700

    Adding comments
    Added comments because I may have to use this later to create a script on only allowing one no label checkbox in a group to be selected.

--- Utilities/requiredGroupedCheckboxes.js
+++ Utilities/requiredGroupedCheckboxes.js
@@ -1,3 +1,4 @@
+// This is to grab all the groups and make sure the groups const only has one instance of each group.
 const groups = {};
 const requiredIndividualCheckboxes = [...document.querySelectorAll('.requireIndividualCheckboxes')];
 requiredIndividualCheckboxes.forEach(element => {
@@ -9,7 +10,7 @@ requiredIndividualCheckboxes.forEach(element => {
 });
 const requiredGroupCheckboxes = Object.values(groups);
 
-
+// This is to add the red asterisk to the main label for the no label checkbox group that is using this script
 requiredGroupCheckboxes.forEach(checkboxGroup => {
 	const asterisk = document.createElement('span');
 	asterisk.textContent = '*';
@@ -20,7 +21,7 @@ requiredGroupCheckboxes.forEach(checkboxGroup => {
 	//});
 });
 
-
+// This is to get the closest <b>. From looking at how the form is structured in html and our style guide, we always put a label before a no label checklist. This is grabbing the <b> for that label.
 var closestB;
 function searchClosestLabel(checkboxGroup){
 	let currentRow = checkboxGroup[0].closest('table').closest('tr');  
@@ -36,35 +37,48 @@ function searchClosestLabel(checkboxGroup){
 	}
 }
 
-
+// This variable is to notify which is the closest group of no label checkboxes does not meet its required checked answer count
 var closestUncheckedRequiredLabel;
+// This variable grab the requirement for the closest group that didn't meet it's requirement. This is used later in a different function.
 var closestUncheckedRequiredCount = 1;
 function checkRequiredGroupCB(){
+    // This variable is to notify which is the closest group of no label checkboxes does not meet its required checked answer count
     closestUncheckedRequiredLabel = null;
+    // These variables are the complete buttons that can show up in a form
     const completeButtonName = document.querySelector('[name=Complete]');
     const completeButtonValue = document.querySelector('[value=Complete]');
     const floatingCompleteButton = document.querySelector('#complete');
+	// This variable is to count how many no label checkboxes in the group are checked
 	let cbCheckedCount = 0;
+	// This variable is to count how groups met their requirement
 	let checkedRequiredGroupCount = 0;
 	requiredGroupCheckboxes.forEach(checkboxGroup => {
+		// This variable is to track how many no label check boxes are required for the group being looked at
 		let requiredCount = 1;
+		// This variable is to count how many no label checkboxes in the group are checked
 		cbCheckedCount = 0;
 		checkboxGroup.forEach(answer => {
+			// This variable is to track how many no label check boxes are required for the group being looked at
 			requiredCount = parseInt(answer.getAttribute('requiredCount'), 10);	
+			//This if statement actually checks if the no label was checked.
 			if(answer.closest('tbody').querySelector('input').checked){
+				// This variable is to count how many no label checkboxes in the group are checked
 				cbCheckedCount = cbCheckedCount + 1;
 			}
 		});
 		if(cbCheckedCount >= requiredCount){
+			// This variable is to count how groups met their requirement
 			checkedRequiredGroupCount = checkedRequiredGroupCount + 1;
 		} 
 		else{
+			// This variable is to notify which is the closest group of no label checkboxes does not meet its required checked answer count 
 			closestUncheckedRequiredLabel = searchClosestLabel(checkboxGroup);
 			closestUncheckedRequiredCount = requiredCount;
 		}
 	});
 	if(checkedRequiredGroupCount < requiredGroupCheckboxes.length){
 		// firstCBUnchecked is from the visibility.js and including it here is to ensure visibility.js and requiredGroupedCheckboxes.js do not overwrite each other
+		// This section is to disable or enable the complete button depending on if all group meet thedir required checked answer count
 		try{
 			if(completeButtonName && firstCBUnchecked == null){
 				completeButtonName.setAttribute('disabled',true);
