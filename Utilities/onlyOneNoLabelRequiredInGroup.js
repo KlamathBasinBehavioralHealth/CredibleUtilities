0927dba2f266e2dd0784b35fff728faf44b9a45b
SBThach <sthach@kbbh.org>
2025-04-09 13:31:13 -0700

    Update onlyOneNoLabelRequiredInGroup.js

--- Utilities/onlyOneNoLabelRequiredInGroup.js
+++ Utilities/onlyOneNoLabelRequiredInGroup.js
@@ -26,12 +26,12 @@ function checkRequiredGroupCB(checkbox_answer){
 				if(answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('textarea')){
 					answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('textarea').required = false;
 					answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.style.display = 'none';
-					//if(checkbox_answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('table').style.display){
-					//	checkbox_answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('table').style.display = 'none';
-					//}
-					//if(checkbox_answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('textarea').closest('td').querySelector('div')){
-					//	checkbox_answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('textarea').closest('td').querySelector('div').remove();
-					//}
+					if(checkbox_answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('table').style.display){
+						checkbox_answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('table').style.display = 'none';
+					}
+					if(checkbox_answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('textarea').closest('td').querySelector('div')){
+						checkbox_answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('textarea').closest('td').querySelector('div').remove();
+					}
 				}			
 			});
 		}
@@ -39,7 +39,7 @@ function checkRequiredGroupCB(checkbox_answer){
 		checkbox_answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.style.display = 'inline';
 		checkbox_answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('table').style.display = 'inline';
 		if(checkbox_answer.classList.contains('requireTextarea')){
-			if(answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('textarea')){
+			if(checkbox_answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('textarea')){
 				checkbox_answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('textarea').required = true;
 				if(checkbox_answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('textarea').closest('td').querySelector('div')){
 					checkbox_answer.closest('tr').closest('tbody').closest('tr').nextElementSibling.querySelector('textarea').closest('td').querySelector('div').remove();
