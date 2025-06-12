64d651d45811a2d83039fd515679b975310e5969
SBThach <sthach@kbbh.org>
2025-06-12 13:51:33 -0700

    Update roiValidDates.js

--- Form Specific Scripts/roiValidDates.js
+++ Form Specific Scripts/roiValidDates.js
@@ -67,16 +67,16 @@ function fillValidDates() {
 	   	if(recipientType.value === payerValue){
 			startDate.value = '';
         	        endDate.value = '';
+			document.querySelector('#validDateStart').closest('tr').nextElementSibling.querySelector('input').require = false;
+			document.querySelector('#validDateEnd').closest('tr').nextElementSibling.querySelector('input').require = false;
+			document.querySelector('#validDateStart').closest('tr').nextElementSibling.querySelector('input').nextElementSibling.style.visibility = 'hidden';
+			document.querySelector('#validDateEnd').closest('tr').nextElementSibling.querySelector('input').nextElementSibling.style.visibility = 'hidden';
 			//visibility('hide', '#validDateStart', false);
 			//visibility('hide', '#validDateEnd', false);
 		} else{
 			startDate.value = today;
 			endDate.value = yearLater;
 			document.querySelector('#validDateStart').closest('tr').nextElementSibling.querySelector('input').readOnly = true;
-			document.querySelector('#validDateStart').closest('tr').nextElementSibling.querySelector('input').nextElementSibling.style.visibility = 'hidden';
-			document.querySelector('#validDateEnd').closest('tr').nextElementSibling.querySelector('input').readOnly = true;
-			document.querySelector('#validDateEnd').closest('tr').nextElementSibling.querySelector('input').nextElementSibling.style.visibility = 'hidden';
-		    	document.querySelector('#validDateStart').closest('tr').nextElementSibling.querySelector('input').readOnly = true;
 			document.querySelector('#validDateStart').closest('tr').nextElementSibling.querySelector('input').nextElementSibling.style.visibility = 'hidden';
 			document.querySelector('#validDateEnd').closest('tr').nextElementSibling.querySelector('input').readOnly = true;
 			document.querySelector('#validDateEnd').closest('tr').nextElementSibling.querySelector('input').nextElementSibling.style.visibility = 'hidden';
