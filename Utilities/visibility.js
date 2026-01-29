589e7d619b97fae427250600dc9d406fc0ae84b0
Yu Hsu <yhsu@kbbh.org>
2026-01-29 11:09:45 -0800

    Updating main visibility function to JavaScript from JQuery.

--- Utilities/visibility.js
+++ Utilities/visibility.js
@@ -86,67 +86,44 @@ function requireNotes (target, condition) {
 function visibility(hideShow, target, require = false){ 
   if(document.querySelector('[name=Complete]')){   
     try{   
-      if(hideShow == 'show'){     
-        $(target).closest('table').closest('tr').each(function (){      
-          $(this).show();       
-          let id;      
-          try{         
-            id = $(this).find('*[id*=q_]').attr('id').slice(2);                  
-            if($('.div' + id)?.length){           
-              $('.div' + id).closest('tr').show();         
-            }         
-            if($(this).next().find('textarea').length){             
-              $(this).next().next().show();         
-            }         
-            else{             
-              $(this).next().show();         
-            }       
-          }       
-          catch(error){                
-            if($(this).next().find('textarea').length){             
-              $(this).next().next().show();         
-            }else{            
-              $(this).next().show();         
-            }       
-          }  
+      if(hideShow == 'show'){    
+        [...document.querySelectorAll('#purpose')].forEach((element) => {
+          element.closest('table').closest('tr').hidden = false;
+          if(element.closest('table').closest('tr').nextElementSibling.querySelector('textarea')){
+            element.closest('table').closest('tr').nextElementSibling.hidden = false;
+            element.closest('table').closest('tr').nextElementSibling.nextElementSibling.hidden = false;
+          }else{
+            element.closest('table').closest('tr').nextElementSibling.hidden = false;
+          }
+          element.setAttribute('visibility', 'show');
         });
-        target.setAttribute('visibility', 'show');
+
+        try{   
+          if(require){   
+            requireField(element, true);   
+          }else{    
+            requireField(element, false);   
+          }   
+        }catch(error){ 
+
+        } 
       }else if(hideShow == 'hide'){     
-        $(target).closest('table').closest('tr').each(function (){       
-          $(this).hide();       
-          let id;       
-          try{         
-            id = $(this).find('*[id*=q_]').attr('id').slice(2);         
-            if($('.div' + id)?.length){           
-              $('.div' + id).closest('tr').hide();         
-            }         
-            if($(this).next().find('textarea').length){            
-              $(this).next().next().hide();         
-            }else{             
-              $(this).next().hide();         
-            }       
-          }catch(error){                  
-            if($(this).next().find('textarea').length){             
-              $(this).next().next().hide();         
-            }else{             
-              $(this).next().hide();         
-            }       
-          }     
+        [...document.querySelectorAll('#purpose')].forEach((element) => {
+          element.closest('table').closest('tr').hidden = true;
+          if(element.closest('table').closest('tr').nextElementSibling.querySelector('textarea')){
+            element.closest('table').closest('tr').nextElementSibling.hidden = true;
+            element.closest('table').closest('tr').nextElementSibling.nextElementSibling.hidden = true;
+          }else{
+            element.closest('table').closest('tr').nextElementSibling.hidden = true;
+          }
+          element.setAttribute('visibility', 'hide');
         });
-        target.setAttribute('visibility', 'hide');
+
+        requireField(element, false);
       }   
     }catch(error){   
  
     }    
-    try{   
-      if(require == true){     
-        requireField(target, true);   
-      }else if (require == false){     
-        requireField(target, false);   
-      }   
-    }catch(error){ 
-
-    } 
   } 
 }  
 
